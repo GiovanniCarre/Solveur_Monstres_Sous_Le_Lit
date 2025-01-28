64feba1e6e6882ba0a6808d1ca94ab87c43bc4cc
Loïc Berthelot <112377978+loic-berthelot@users.noreply.github.com>
2025-01-28 20:59:47 +0100

    Update Main4.py

--- Main4.py
+++ Main4.py
@@ -2,13 +2,6 @@ from pydoc import describe
 
 from pycsp3 import *
 
-def countZeros(tableau):
-    count = 0
-    for masque in tableau:
-        for ligne in masque:
-            count += ligne.count(0)
-    return count
-
 def rightRotation(grid):
     grid2 = [[0,0,0],[0,0,0],[0,0,0]]
     grid2[1][1] = grid[1][1]
@@ -18,11 +11,6 @@ def rightRotation(grid):
         grid2[2][i] = grid[2-i][2]
         grid2[2-i][2] = grid[0][2-i]
     return grid2
-
-def display(grid):
-    for ligne in grid:
-        print(ligne)
-    print("")
 
 def rotate(grid, n):
     for i in range(n):
@@ -50,8 +38,8 @@ def creerChambres():
         chambres.append(flatten(chambre))
     return chambres
 
-def crerMasquesOriginaux() :
-    masquesOriginauxFixes = \
+def crerMasques() :
+    masquesFixes = \
         [[[0, 1, 0],
         [0, 0, 0],
         [0, 1, 0]],
@@ -68,78 +56,55 @@ def crerMasquesOriginaux() :
         [0, 0, 0],
         [0, 1, 0]]]
 
-    masquesOriginaux = []
+    masques = []
     for i in range(4):
         for j in range(4):
-            tab = rotate(masquesOriginauxFixes[i], j)
+            tab = rotate(masquesFixes[i], j)
             tab.append(i)
-            masquesOriginaux.append(tab)
-
-    return masquesOriginaux, countZeros(masquesOriginauxFixes)
-
-def fonctionDeDorian(objectif):
-    sommeNombreMonstres = 0
-    for i in objectif:
-        sommeNombreMonstres += i
-    objectif[0] = nMasquesOrginauxFixes + objectif[0]
-    print("Nombre de cases vides: ", objectif[0])
+            if not tab in masques :
+                masques.append(tab)
+    return masques
 
 def afficherSolution(sol):
-    #print(sol)
     for i in range(4):
-        description = "Le masque " + str(i) + " est placé sur la chambre " + str(sol[i]["position"]) + " et est de la forme :"
+        description = "Sur la chambre " + str(i) + " est placé le masque " + str(sol[i][9]) + " :"
         for j in range(9):
             if (j%3==0) :
                 description+="\n"
-            description+= str(sol[i]["rotations"][j])+", "
+            description+= str(sol[i][j])+", "
         description = description[:-2]
         print(description)
 
 if __name__ == "__main__":
     nbCategoriesDeMonstres = 7
     chambres = creerChambres()
-
-    masquesOriginaux, nMasquesOrginauxFixes = crerMasquesOriginaux()
-    nMasques = len(masquesOriginaux)
+    masques = crerMasques()
+    nMasques = len(masques)
     nombreMontresObjectif = [0, 0, 0, 2, 0, 0, 0, 3, 0]#3 singes et 2 yetis (essayer aussi [0, 1, 1, 0, 1, 1, 0, 1, 0])
 
-    #fonctionDeDorian(nombreMontresObjectif)
-
-    positionMasques = VarArray(size=4, dom=range(4))
-    selectionMasques = VarArray(size=4, dom=range(nMasques))
-    masquesOriginauxVar = VarArray(size=[nMasques,10], dom=range(5))
+    masquesVar = VarArray(size=[nMasques,10], dom=range(5))
     masquesSelectionnes = VarArray(size=[4,10], dom=range(5))
+    selectionMasques = VarArray(size=4, dom=range(nMasques))
     monstresVisibles = VarArray(size=[4,9], dom=range(nbCategoriesDeMonstres+1))
     satisfy(
-        (masquesOriginauxVar[i] == masquesOriginaux[i] for i in range(nMasques)),
-        (masquesSelectionnes[i] == masquesOriginauxVar[selectionMasques[i]] for i in range(4)),
-        (positionMasques[i] == masquesSelectionnes[i][9] for i in range(4)),
+        (masquesVar[i] == masques[i] for i in range(nMasques)),
+        (masquesSelectionnes[i] == masquesVar[selectionMasques[i]] for i in range(4)),
+        (AllDifferent([masquesSelectionnes[i][9] for i in range(4)])),
         (monstresVisibles[i][j] == chambres[i][j]*masquesSelectionnes[i][j] for i in range(4) for j in range(9)),
-        (AllDifferent(positionMasques)),
         (Count(monstresVisibles, value=i) == nombreMontresObjectif[i] for i in range(1, nbCategoriesDeMonstres + 1))
     )
 
-    solutions = []
-    nSolutionsTrouves = 0
-    maxSolutions = 3
-    while solve() is SAT and nSolutionsTrouves < maxSolutions:
-        nSolutionsTrouves += 1
+    if solve(sols=ALL) is SAT and n_solutions():
         solution = []
-        for i in range(4):
-            masque = {}
-            masque["position"]=i
-            masque["rotations"] = []
-            for j in range(9):
-                masque["rotations"].append(value(monstresVisibles[i][j]))
-            solution.append(masque)
-        if (solution in solutions) :
-            print("SAT, solution déjà trouvée")
-        else :
-            print("SAT")
-            solutions.append(solution)
-
-    print("\n--- AFFICHAGE DES SOLUTIONS ---")
-    for i, sol in enumerate(solutions) :
-        print("Solution "+str(i)+" : ")
-        afficherSolution(sol)
-        print("\n")
+        print("SAT avec ", n_solutions(), " solutions")
+        for k in range(n_solutions()):
+            for i in range(4):
+                chambre = []
+                for j in range(10):
+                    chambre.append(value(masquesSelectionnes[i][j], sol=k))
+                solution.append(chambre)
+            print("\n--- AFFICHAGE DE LA SOLUTION "+str(k)+" ---")
+            afficherSolution(solution)
+            print("\n")
+    else :
+        print("UNSAT")
